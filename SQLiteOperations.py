2719a093674993719d215b5bb5e709c2c2523a48
Matheus Ortiz <130514126+MizashiN@users.noreply.github.com>
2024-11-05 10:43:20 -0300

    Atualizar o SQLiteOperations.py

--- SQLiteOperations.py
+++ SQLiteOperations.py
@@ -1,31 +1,61 @@
-import sqlite3
-
+import mysql.connector
+from mysql.connector import Error
 
 class Operations:
     def __init__(self):
-        self.conn = sqlite3.connect("database.db")
-        self.cursor = self.conn.cursor()
-        self.list = []
+        self.conn = None
+        self.cursor = None
+        self.host = "gym-app.mysql.uhserver.com"
+        self.username = "parafalpwladmin"
+        self.password = "@D3adlift"
+        self.database = "gym_app"
+        self.connect()
 
-    def VerifyImages(self, src_list):
+    def connect(self):
+        try:
+            self.conn = mysql.connector.connect(
+                host=self.host,
+                user=self.username,
+                password=self.password,
+                database=self.database
+            )
+            if self.conn.is_connected():
+                self.cursor = self.conn.cursor()
+            else:
+                print("Failed to connect to the database.")
+        except Error as e:
+            print(f"Error while connecting to MySQL: {e}")
+
+    def verify_images(self, src_list):
+        if self.conn is None or not self.conn.is_connected():
+            self.connect()
         self.list = src_list.copy()
+
         for image_src in src_list:
             self.cursor.execute(
-                "SELECT image_src FROM images WHERE image_src = :image_src",
-                {"image_src": image_src},
+                "SELECT image_src FROM images WHERE image_src = %s", (image_src,)
             )
             result = self.cursor.fetchone()
             if result:
                 self.list.remove(image_src)
-
         return self.list
 
-    def InsertImg(self, image_src, image_blob):
-        self.cursor.execute(
-            "INSERT INTO IMAGES (image_src, image_blob) VALUES (:image_src, :image_blob)",
-            {"image_src": image_src, "image_blob": image_blob},
-        )
-        self.conn.commit()
+    def insert_img(self, image_src, image_blob):
+        if self.conn is None or not self.conn.is_connected():
+            self.connect()
+        try:
+            self.cursor.execute(
+                "INSERT INTO images (image_src, image_blob) VALUES (%s, %s)",
+                (image_src, image_blob)
+            )
+            self.conn.commit()
+        except mysql.connector.OperationalError as e:
+            print(f"Operational error: {e}")
+        except Error as e:
+            print(f"Error: {e}")
 
     def close(self):
-        self.conn.close()
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
