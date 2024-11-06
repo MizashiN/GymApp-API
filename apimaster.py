a7b45a5bdd740ed89137045b97b086a49e6f16fc
Matheus Ortiz <130514126+MizashiN@users.noreply.github.com>
2024-11-06 12:23:33 -0300

    Update apimaster.py

--- apimaster.py
+++ apimaster.py
@@ -1,91 +1,17 @@
-from flask import Flask, jsonify, request
 from product_scraper import *
-from generic_funcs import funcs
+from SQLiteOperations import Operations
 
+class Commands:
+    def __init__(self):
+        self.operation = Operations()
+        self.response = All()
+        self.categories = []
+        
+    def Start(self):
+        self.categories = self.operation.SelectCategories
+        for category in self.categories:
+            self.response.set(category)
+            
 
-app = Flask(__name__)
-
-funcs_instance = funcs()
-
-
-class API:
-    @app.route("/maxtitanium", methods=["GET"])
-    def get_supp_MaxTitanium():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory", "")
-
-        response = MaxTitanium()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-
-    @app.route("/adaptogen", methods=["GET"])
-    def get_supp_Adaptogen():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = Adaptogen()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-
-    @app.route("/darklab", methods=["GET"])
-    def get_supp_Darklab():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = DarkLab()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-    @app.route("/mith", methods=["GET"])
-    def get_supp_Mith():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = Mith()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-
-    @app.route("/growthsupp", methods=["GET"])
-    def get_supp_GrowthSupp():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = GrowthSupp()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-    
-    @app.route("/darkness", methods=["GET"])
-    def get_supp_Darkness():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = Darkness()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-    
-
-    @app.route("/all", methods=["GET"])
-    def get_supp_proteins():
-
-        category = request.args.get("category")
-        subcategory = request.args.get("subcategory")
-
-        response = All()
-        result = response.set(category, subcategory)
-
-        return jsonify(result)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+instance = Commands()
+instance.Start()
