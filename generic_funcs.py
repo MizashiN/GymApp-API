4f9d4d9d3fd308d01983d66a0bee5357ecb47050
Matheus Ortiz <matheusbasket22@gmail.com>
2024-11-06 00:09:00 -0300

    a

--- generic_funcs.py
+++ generic_funcs.py
@@ -1,22 +1,20 @@
 import requests
 from PIL import Image
-import os 
+import os
 from SQLiteOperations import Operations
+
 
 class funcs:
     def __init__(self) -> None:
-        self.input_path = os.path.join(
-            "input_image_path" 
-        )
-        self.output_path = os.path.join(
-            "output_image_path"  # Diretório de saída
-        )
+        self.input_path = os.path.join("input_image_path")
+        self.output_path = os.path.join("output_image_path")  # Diretório de saída
         self.operation = Operations()
+
     def download_images(self, urls):
         save_directory = "input_image_path"
 
         for self.url in urls:
-            file_name = os.path.basename(self.url) + '.png' 
+            file_name = os.path.basename(self.url) + ".png"
             file_name = file_name.replace("?", "").replace("&", "")
             save_path = os.path.join(save_directory, file_name)
 
@@ -32,13 +30,14 @@ class funcs:
                     )
             except Exception as e:
                 print(f"Ocorreu um erro ao baixar a imagem {self.url}: {e}")
-        
-            self.get_icon_img(input_image_path=self.input_path, output_path=self.output_path)
+
+            self.get_icon_img(
+                input_image_path=self.input_path, output_path=self.output_path
+            )
             self.blob = self.GetBlobImg(path=self.output_path, filename=file_name)
             self.operation.insert_img(self.url, self.blob)
-            self.DeleteFiles(input_path=self.input_path ,output_path=self.output_path)
+            self.DeleteFiles(input_path=self.input_path, output_path=self.output_path)
 
-        
     def get_icon_img(self, input_image_path, output_path):
         # Verifica se o diretório de entrada existe
         if not os.path.isdir(input_image_path):
@@ -84,42 +83,34 @@ class funcs:
                     output_image_with_background.save(output_file_path, format="PNG")
                     print(f"Imagem salva em: {output_file_path}")
                 except Exception as e:
-                     print(f"Erro ao processar a imagem {filename}: {e}")
-    
+                    print(f"Erro ao processar a imagem {filename}: {e}")
+
     def GetBlobImg(self, path, filename):
 
         full_image_path = os.path.join(path, filename)
-        with open(full_image_path, 'rb') as file:
+        with open(full_image_path, "rb") as file:
             blob_data = file.read()
 
         return blob_data
-    
+
     def DeleteFiles(self, input_path, output_path):
         # Lista todos os arquivos na pasta e os remove
         for arquivo in os.listdir(input_path):
             full_path = os.path.join(input_path, arquivo)
-            
+
             # Verifica se é um arquivo (ignora diretórios)
             if os.path.isfile(full_path):
                 os.remove(full_path)  # Remove o arquivo
                 print(f"Arquivo '{arquivo}' removido em {input_path}.")
             else:
                 print(f"'{arquivo}' não é um arquivo, ignorado.")
-        
+
         for arquivo in os.listdir(output_path):
             full_path = os.path.join(output_path, arquivo)
-            
+
             # Verifica se é um arquivo (ignora diretórios)
             if os.path.isfile(full_path):
                 os.remove(full_path)  # Remove o arquivo
                 print(f"Arquivo '{arquivo}' removido em {output_path}.")
             else:
                 print(f"'{arquivo}' não é um arquivo, ignorado.")
-
-
-
-                        
-
-        
-
-    
